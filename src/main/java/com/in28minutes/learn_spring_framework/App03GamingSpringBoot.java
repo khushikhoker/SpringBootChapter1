c4a7497ef0b706664c0f97c61d4b76a3724e571b
Khushi <khushikhoker9@gmail.com>
2025-08-06 19:25:05 +0530

    @Primary and @Qualifier annotations

--- src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBoot.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBoot.java
@@ -4,23 +4,12 @@ import com.in28minutes.learn_spring_framework.game.GamingConsole;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.in28minutes.learn_spring_framework")
 public class App03GamingSpringBoot {
-
-    @Bean
-    public GamingConsole game() {
-        var gameconsole = new MarioGame();
-        return gameconsole;
-    }
-
-    @Bean
-    public GameRunner gameRunner(GamingConsole game) {
-        var runner = new GameRunner(game);
-        return runner;
-    }
-
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(App03GamingSpringBoot.class)) {
             context.getBean(GameRunner.class).run();
--- src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
@@ -1,11 +1,14 @@
 package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GameRunner {
     GamingConsole game;
 
-    public GameRunner(GamingConsole game) {
+    public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game) {
         this.game = game;
     }
 
